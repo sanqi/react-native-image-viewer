bc6c06e3a246cbe51e6e43a8eefa50f55de6e89a
SUIFENG <suifeng@gmail.com>
2021-06-13 00:33:01 +0800

    Update image-viewer.component.tsx

--- src/image-viewer.component.tsx
+++ src/image-viewer.component.tsx
@@ -186,6 +186,9 @@ export default class ImageViewer extends React.Component<Props, State> {
 
     // 如果已知源图片宽高，直接设置为 success
     if (image.width && image.height) {
+      if (this.props.enablePreload && imageLoaded === false) {
+        Image.prefetch(image.url);
+      }
       imageStatus.width = image.width;
       imageStatus.height = image.height;
       imageStatus.status = 'success';
@@ -197,8 +200,9 @@ export default class ImageViewer extends React.Component<Props, State> {
       CachedImage.cacheDir = this.props.cacheDir;
     }
     // @ts-ignore: Unreachable code error
-    CachedImage.getSize(
+    Image.prefetchWithHeaders(
       image.url,
+      image!.props!.source!.headers,
       (width: number, height: number) => {
         imageStatus.width = width;
         imageStatus.height = height;
